4165f2443b306d770ba588c33c7dc21f51e21925
ochosteve <ochosteve08@gmail.com>
2023-07-05 10:03:18 +0100

    create model schema

---
+++ src/models/todo.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const todoModel = mongoose.model("todo", todoSchema);
+module.exports = todoModel;
