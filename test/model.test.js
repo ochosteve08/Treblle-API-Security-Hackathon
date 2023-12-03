78e20e29901a90332aae1b7077a1f7e4ed565636
ochosteve <ochosteve08@gmail.com>
2023-12-03 07:39:43 +0100

    integrate test for  register,login user, add todo, cleanup todo

--- test/model.test.js
+++ test/model.test.js
@@ -1,10 +1,27 @@
+const { todoModel } = require("../src/models");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const {todoModel} = require('../src/models')
+beforeAll(async function () {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 50000,
+    });
+    console.log("Connected to test database");
+  } catch (error) {
+    console.error("Error connecting to test database:", error);
 
+  }
+});
 
+afterAll(async function () {
+  await mongoose.connection.close();
+  console.log("Disconnected from test database");
+});
 
 
 describe("todo schema validate", () => {
+
   it("should validate todo model schema", async () => {
     const newTodo = new todoModel();
 
@@ -12,6 +29,33 @@ describe("todo schema validate", () => {
       await newTodo.validate();
     } catch (error) {
       expect(error.errors.title).toBeDefined();
+      expect(error.errors.description).toBeDefined();
+    }
+  });
+});
+
+
+describe("Testing Item model", () => {
+  let sampleItemVal;
+
+  beforeEach(() => {
+    sampleItemVal = {
+      title: "sample item",
+      description: 'happy day',
+      userId: "5",
+      completed: true,
+    };
+  });
+
+  it("it should throw an error due to missing fields", async () => {
+    const todo = new todoModel();
+
+    try {
+      await todo.validate();
+    } catch (error) {
+      expect(error.errors.title).toBeDefined();
+      expect(error.errors.description).toBeDefined();
+      expect(error.errors.userId).toBeDefined();
     }
   });
 });
--- test/route.test.js
+++ test/route.test.js
@@ -1,20 +1,63 @@
 const request = require("supertest");
 const app = require("../index");
-// const { connectToMongoDb } = require("../src/config");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+beforeAll(async function () {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 50000,
+    });
+    console.log("Connected to test database");
+  } catch (error) {
+    console.error("Error connecting to test database:", error);
+    process.exit(1);
+  }
+});
+
+afterAll(async function () {
+  await mongoose.connection.close();
+  console.log("Disconnected from test database");
+});
 
 describe("TODOS API", () => {
-//     beforeAll(async () => {
-//       try {
-//         await connectToMongoDb();
-//         console.info("Connected to MongoDB");
-//       } catch (error) {
-//         console.error("Error in connecting to MongoDB", error);
-//         process.exit(1);
-//       }
-//     });
+  it("GET /test should return a status code of 200", async () => {
+    const Response = await request(app).get("/test");
+    expect(Response.status).toBe(200);
+  });
 
-   it("GET /test should return a status code of 200", async () => {
-     const response = await request(app).get("/test");
-     expect(response.status).toBe(200);
-   });
+  it("should Register user, login user, check token and create", async function () {
+    const registerResponse = await request(app)
+      .post("/api/v1/users/register")
+      .send({
+        email: "tester@gmail.com",
+        password: "Professor-08",
+      });
+    expect(registerResponse.status).toBe(200);
+
+    const loginResponse = await request(app).post("/api/v1/users/login").send({
+      email: "tester@gmail.com",
+      password: "Professor-08",
+    });
+    expect(loginResponse.status).toBe(200);
+    expect(loginResponse.body.data).toHaveProperty("token");
+    expect(loginResponse.body.data).toHaveProperty("user");
+    const token = loginResponse.body.data.token;
+
+    const createTodoResponse = await request(app)
+      .post("/api/v1/todos")
+      .send({
+        title: "sample item",
+        description: "happy day",
+      })
+      .set("Authorization", `Bearer ${token}`);
+    expect(createTodoResponse.status).toBe(200);
+    console.log(createTodoResponse.body.data);
+
+    const todoId = createTodoResponse.body.data.todo._id;
+    const cleanupResponse = await request(app)
+      .delete(`/api/v1/todos/${todoId}`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(cleanupResponse.status).toBe(200);
+  });
 });
